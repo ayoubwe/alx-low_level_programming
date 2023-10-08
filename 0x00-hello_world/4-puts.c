b0f8ce3f9fc7ef01db7dedfbd49edbd388ad535f
Ayoub Douane <douaneayoubw@gmail.com>
2023-10-08 16:43:36 +0000

    sss

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ * main-purts,Programmin is like building a multilingual puzzle
+ * return 0
+ * */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints with proper grammar, but the outcome is a piece of art,
- * Return: 0
+ * return: 0
 **/
 
 int main(void)
