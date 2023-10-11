cdb10e739255c3a9ba373f80d5a030316f5bb5ad
Ayoub Douane <douaneayoubw@gmail.com>
2023-10-11 23:45:03 +0000

    I fixed that

---
+++ 0x02-functions_nested_loops/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -Prints the alphabetic
+ * Retun :0
+ */
+int main (void)
+
+{
+	chat c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n'); 
+	return (0);
+}
