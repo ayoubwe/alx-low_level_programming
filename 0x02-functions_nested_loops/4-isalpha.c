e87cb8d71b8477cac374226d15b6291013a12cc7
Ayoub Douane <douaneayoubw@gmail.com>
2023-10-12 00:15:06 +0000

    sss

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,10 @@
 #include "holberton.h"
 /**
- * _isalpha - write a function that checks for alphabetic character.
+ * _isalpha - Check Main
+ * @c: An input character
+ * Description: function returns 1 if the character is a
+ * letter, lowercase or uppercase.
+ * Return: 1 or 0 in otherwise.
  */
 int _isalpha(int c)
 {
