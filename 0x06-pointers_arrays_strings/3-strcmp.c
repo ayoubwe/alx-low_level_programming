0164bc8889fcc2e9e12af781810c0019dff02bd6
Ayoub Douane <douaneayoubw@gmail.com>
2023-10-19 19:40:37 +0000

    ss

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strcmp - 2 string
+ * @s1:char
+ * @s2:char
+ * Return:s1 - s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (*s1 - *s2);
+	}
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+0x06. C - More pointers, arrays and strings
+We can using gcc, using the options -Wall -Werror -Wextra -pedantic -std=gnu89, for this project.
