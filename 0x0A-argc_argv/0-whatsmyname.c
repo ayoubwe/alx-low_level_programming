2772bff2d8617a79a43f028c39d3a3be5b633184
Ayoub Douane <douaneayoubw@gmail.com>
2023-10-31 10:06:24 +0000

    ss

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints its name, followed by a new line.
+ * @argc: int
+ * @argv: char
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+The project of 0x0A. C - argc, argv
+we use this functions; -Wall -Werror -Wextra -pedantic -std=gnu89
