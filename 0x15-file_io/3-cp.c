f0c82d53ad60967244a1abf445aa109016c6b8eb
Ayoub Douane <douaneayoubw@gmail.com>
2023-12-01 20:05:26 +0000

    ss

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,73 +1,109 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * error_file - checks.
- * @file_from: int
- * @file_to: int
- * @argv: char
+ * check97 - checks for the correct nmb
+ * @argc: int
  * Return: Nothing
  */
-void error_file(int file_from, int file_to, char *argv[])
+void check97(int argc)
 {
-	if (file_from == -1)
+	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Error: You can't read from %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+}
+
+/**
+ * check98 - checks if can be read
+ * @check: ssize_t
+ * @file: char
+ * @fd_from: int
+ * @fd_to: int
+ * Return: Nothing
+ */
+void check98(ssize_t check, char *file, int fd_from, int fd_to)
+{
+	if (check == -1)
+	{
+		dprintf(STDERR_FILENO, "Error:You can't read from file %s\n", file);
+		if (fd_from != -1)
+			close(fd_from);
+		if (fd_to != -1)
+			close(fd_to);
 		exit(98);
 	}
-	if (file_to == -1)
+}
+
+/**
+ * check99 - checks that file_to
+ * @check: ssize_t
+ * @file: char
+ * @fd_from: int
+ * @fd_to: int
+ * Return: Nothing
+ */
+void check99(ssize_t check, char *file, int fd_from, int fd_to)
+{
+	if (check == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: You can't write %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		if (fd_from != -1)
+			close(fd_from);
+		if (fd_to != -1)
+			close(fd_to);
 		exit(99);
 	}
 }
 
 /**
- * main - check the code.
+ * check100 - checks that file
+ * @check: int
+ * @fd: int
+ * Return: Nothing
+ */
+void check100(int check, int z)
+{
+	if (check == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", z);
+		exit(100);
+	}
+}
+/**
+ * main - the content of a file
  * @argc: int
  * @argv: char
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, err_close;
-	ssize_t nchars, nwr;
-	char buf[1024];
+	int fd_from, fd_to, close_to, close_from;
+	ssize_t lenr, lenw;
+	char buffer[1024];
+	mode_t file_perm;
 
-	if (argc != 3)
+	check97(argc);
+	fd_from = open(argv[1], O_RDONLY);
+	check98((ssize_t)fd_from, argv[1], -1, -1);
+	file_perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_perm);
+	check99((ssize_t)fd_to, argv[2], fd_from, -1);
+	lenr = 1024;
+	while (lenr == 1024)
 	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: the cp file_from file_to");
-		exit(97);
+		lenr = read(fd_from, buffer, 1024);
+		check98(lenr, argv[1], fd_from, fd_to);
+		lenw = write(fd_to, buffer, lenr);
+		if (lenw != lenr)
+			lenw = -1;
+		check99(lenw, argv[2], fd_from, fd_to);
 	}
-
-	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_file(file_from, file_to, argv);
-
-	nchars = 1024;
-	while (nchars == 1024)
-	{
-		nchars = read(file_from, buf, 1024);
-		if (nchars == -1)
-			error_file(-1, 0, argv);
-		nwr = write(file_to, buf, nchars);
-		if (nwr == -1)
-			error_file(0, -1, argv);
-	}
-
-	err_close = close(file_from);
-	if (err_close == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: You can't close fd %d\n", file_from);
-		exit(100);
-	}
-
-	err_close = close(file_to);
-	if (err_close == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: You can't close fd %d\n", file_from);
-		exit(100);
-	}
+	close_to = close(fd_to);
+	close_from = close(fd_from);
+	check100(close_to, fd_to);
+	check100(close_from, fd_from);
 	return (0);
 }
-
