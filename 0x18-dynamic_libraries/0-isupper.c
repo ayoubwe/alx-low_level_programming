35d4f2b2c272232d0066199f87025b2a30d0e704
Ayoub Douane <douaneayoubw@gmail.com>
2024-01-20 01:01:02 +0100

    ss

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - checks fo uppers
+ * @c: be checked
+ * Return: 1 or 0
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x18-dynamic_libraries/README.md
@@ -0,0 +1,1 @@
+My project of 0x18.C -Dynamic libraries
